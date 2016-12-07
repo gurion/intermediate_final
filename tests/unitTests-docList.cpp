388b610bc2e5744d80c933e2048a00f514edab8c
Gurion Marks <gurion@jhu.edu>
2016-12-07 13:01:09 -0500

    fixed docLIst test

--- tests/unitTests-docList.cpp
+++ tests/unitTests-docList.cpp
@@ -28,14 +28,14 @@ TEST_CASE("checkFileList", "[checkFileList]") {
 TEST_CASE("makeList", "[makeList]") {
   docList doclist1;
   vector<string> list1 = doclist1.makeList("testfile1.txt");
-  CATCH(doclist1.numDocs == 3);
+  CATCH(doclist1.getNumDocs() == 3);
   CATCH(list1 == {"test1doc1.txt", "test1doc2.txt", "test1doc3.txt"});
   docList doclist2;
   vector<string> list2 = doclist2.makeList("testfile2.txt");
-  CATCH(doclist2.numDocs == 0);
+  CATCH(doclist2.getNumDocs() == 0);
   CATCH(list2 == {""});
   docList doclist3;
   vector<string> list3 = doclist3.makeList("testfile3.txt");
-  CATCH(doclist3.numDocs == 5);
+  CATCH(doclist3.getNumDocs() == 5);
   CATCH(list3 == {"test3doc1.txt", "test3doc2.txt", "test3doc3.txt", "test3doc4.txt", "test3doc5.txt"});
 }
