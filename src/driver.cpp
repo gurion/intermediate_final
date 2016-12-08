b499db5f1f1d8b0c1c0afc6ea634ccb5468b30f7
Jeremy Mazumder <jmazumder66@gmail.com>
2016-12-07 20:40:12 -0500

    Fixed indents in driver.cpp

--- src/driver.cpp
+++ src/driver.cpp
@@ -8,33 +8,33 @@ using std::cout;
 using std::cin;
 
 int main(int argc, char* argv[]){
-	string filestring;
-	char choiceChar;
-	string output;
-	cout << "\nWelcome to Gurion's Plagiarism Checker.\n\n";
-	if (argc > 3 || argc <= 1){
-		cout << "ERROR - Invalid arguments.\n Goodbye!\n\n";
-		return 0;
-	}
-	if (argc == 2){
-		filestring = argv[1];
-		choiceChar = 'm';
-	}
-	if (argc == 3){
-		filestring = argv[1];
-		choiceChar = argv[2][0];
-		choiceChar = tolower(choiceChar);
-		if (choiceChar != 'h' && choiceChar != 'm' && choiceChar != 'l'){
-			cout << "Invalid sensitivity, program terminated. Goodbye!";
-		}
-	}
-
-	output = algs(filestring, choiceChar);
-
-	if (output != "ERROR - Bad file input.")
-		cout << "The following pairs of files have been marked as suspiciously similar: \n\n";
-	
-	cout << output;
-
-	return 0;
+  string filestring;
+  char choiceChar;
+  string output;
+  cout << "\nWelcome to Gurion's Plagiarism Checker.\n\n";
+  if (argc > 3 || argc <= 1){
+    cout << "ERROR - Invalid arguments.\n Goodbye!\n\n";
+    return 0;
+  }
+  if (argc == 2){
+    filestring = argv[1];
+    choiceChar = 'm';
+  }
+  if (argc == 3){
+    filestring = argv[1];
+    choiceChar = argv[2][0];
+    choiceChar = tolower(choiceChar);
+    if (choiceChar != 'h' && choiceChar != 'm' && choiceChar != 'l'){
+      cout << "Invalid sensitivity, program terminated. Goodbye!";
+    }
+  }
+  
+  output = algs(filestring, choiceChar);
+  
+  if (output != "ERROR - Bad file input.")
+    cout << "The following pairs of files have been marked as suspiciously similar: \n\n";
+  
+  cout << output;
+  
+  return 0;
 }
