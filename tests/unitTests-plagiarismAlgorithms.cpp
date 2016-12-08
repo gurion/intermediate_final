5fddca22984bbf49a74d03927cba9dd42ed785f9
Jeremy Mazumder <jmazumder66@gmail.com>
2016-12-07 22:03:35 -0500

    Changed unit tests for plagiarism algorithms

--- tests/unitTests-plagiarismAlgorithms.cpp
+++ tests/unitTests-plagiarismAlgorithms.cpp
@@ -14,7 +14,7 @@ using std::list;
 using std::ifstream;
 
 TEST_CASE("testfile1", "[algs],[compare]") {
-  CHECK(algs("testfile1.txt", 'l') == "test1doc1.txt test1doc2.txt\n");
+  CHECK(algs("testfile1.txt", 'l') == "");
   CHECK(algs("testfile1.txt", 'm') == "test1doc1.txt test1doc2.txt\n");
   CHECK(algs("testfile1.txt", 'h') == "test1doc1.txt test1doc2.txt\ntest1doc1.txt test1doc3.txt\n");
 }
@@ -22,6 +22,6 @@ TEST_CASE("testfile1", "[algs],[compare]") {
 
 TEST_CASE("testfile3", "[algs],[compare]") {
   CHECK(algs("testfile3.txt", 'l') == "test3doc1.txt test3doc5.txt\n");
-  CHECK(algs("testfile3.txt", 'm') == "test3doc1.txt test3doc");
-  CHECK(algs("testfile3.txt", 'h') == "");
+  CHECK(algs("testfile3.txt", 'm') == "test3doc1.txt test3doc3.txt\ntest3doc1.txt test3doc4.txt\ntest3doc1.txt test3doc5.txt\n");
+  CHECK(algs("testfile3.txt", 'h') == "test3doc1.txt test3doc2.txt\ntest3doc1.txt test3doc3.txt\ntest3doc1.txt test3doc4.txt\ntest3doc1.txt test3doc5.txt\n");
 }
